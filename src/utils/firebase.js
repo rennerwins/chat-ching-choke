968fc6678c858f509a8b1ac2251ebdf68e50327b
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2017-08-08 15:04:01 +0700

    Add gitignore

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,7 +1,12 @@
 import * as firebase from 'firebase'
 
 const config = {
-	// Put your firebase config here
+	apiKey: 'AIzaSyAShU7XQD5ji6DDf7PY__EUGb9LwvukrNU',
+	authDomain: 'codelab-a8367.firebaseapp.com',
+	databaseURL: 'https://codelab-a8367.firebaseio.com',
+	projectId: 'codelab-a8367',
+	storageBucket: 'codelab-a8367.appspot.com',
+	messagingSenderId: '565799047733'
 }
 
 export const firebaseApp = firebase.initializeApp(config)
