2c077856917d805d88cab3c73d33b358da1025fa
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2017-08-08 15:16:18 +0700

    test

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase'
 
 const config = {
-	// apiKey: 'AIzaSyAShU7XQD5ji6DDf7PY__EUGb9LwvukrNU',
-	// authDomain: 'codelab-a8367.firebaseapp.com',
-	// databaseURL: 'https://codelab-a8367.firebaseio.com',
-	// projectId: 'codelab-a8367',
-	// storageBucket: 'codelab-a8367.appspot.com',
-	// messagingSenderId: '565799047733'
+	apiKey: 'AIzaSyAShU7XQD5ji6DDf7PY__EUGb9LwvukrNU',
+	authDomain: 'codelab-a8367.firebaseapp.com',
+	databaseURL: 'https://codelab-a8367.firebaseio.com',
+	projectId: 'codelab-a8367',
+	storageBucket: 'codelab-a8367.appspot.com',
+	messagingSenderId: '565799047733'
 }
 
 export const firebaseApp = firebase.initializeApp(config)
