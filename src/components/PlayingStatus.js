1f7380b1905b0238480e9b7569514c0888fb5ffc
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2017-08-25 10:32:08 +0700

    Change greeting status

--- src/components/PlayingStatus.js
+++ src/components/PlayingStatus.js
@@ -3,13 +3,30 @@ import React from 'react'
 const PlayingStatus = props => {
 	let { canEnter, playing } = props
 	return (
-		<h4>
-			{canEnter && playing && 'กิจกรรมเริ่มแล้ว'}
+		<div>
+			{canEnter && playing && <h4>กิจกรรมเริ่มแล้ว</h4>}
 
-			{canEnter && !playing && 'กิจกรรมกำลังจะเริ่ม'}
+			{canEnter && !playing && <h4>กิจกรรมกำลังจะเริ่ม</h4>}
 
-			{!canEnter && !playing && 'กิจกรรมยังไม่เริ่ม ไว้เจอกันวันจันทร์ 2 ทุ่ม นะ'}
-		</h4>
+			{!canEnter &&
+				!playing &&
+				<div>
+					<h5>
+						ยินดีต้อนรับเข้าสู่เกมแชทชิงโชค จันทร์นี้ 2 ทุ่ม
+						เพียงตอบคำถามขั้นต่ำ 3 ข้อ ทาง{' '}
+						<a href="https://youtube.com/droidsans">youtube.com/droidsans</a>{' '}
+						ก็มีสิทธิ์ร่วมลุ้นรับเครื่อง Galaxy Note 8 ฟรี!
+					</h5>
+
+					<p>
+						กติกาอ่านเพิ่มเติมได้ที่{' '}
+						<a href="https://droidsans.com/chatchingchoke-august-note8/">
+							https://droidsans.com/chatchingchoke-august-note8/
+						</a>
+						อย่าลืมเข้ามาร่วมสนุกกับพวกเรานะ
+					</p>
+				</div>}
+		</div>
 	)
 }
 
