43116665213d11ffef8f06753ddfa3bba93a4ec6
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2017-09-08 13:21:09 +0700

    [Admin] Add menubar

---
+++ src/components/Admin/Menu/Menubar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const DrawerWrapper = styled.div``
+
+const Menubar = () => {
+	return (
+		<DrawerWrapper>
+			<div>test</div>
+		</DrawerWrapper>
+	)
+}
+
+export default Menubar
--- src/containers/admin/Admin.js
+++ src/containers/admin/Admin.js
@@ -8,22 +8,38 @@ import AdminWinner from './AdminWinner'
 import AdminUsers from './AdminUsers'
 import AdminPrize from './AdminPrize'
 import AdminCoupon from './AdminCoupon'
+import Menubar from '../../components/Admin/Menu/Menubar'
+import styled from 'styled-components'
+
+const AdminMenu = styled.div`
+	background-color: #fff;
+	z-index: 2;
+	box-shadow: 2px 0px 10px rgba(0, 0, 0, 0.3);
+`
+const AdminActions = styled.div`background-color: red;`
+const AdminWrapper = styled.div`height: 100%;`
 
 class Admin extends Component {
 	render() {
 		return (
-			<BrowserRouter>
-				<Switch>
-					<Route exact path="/admin" component={AdminMain} />
-					<Route path="/admin/quiz" component={AdminQuiz} />
-					<Route path="/admin/participants" component={AdminParticipants} />
-					<Route path="/admin/scores" component={AdminScores} />
-					<Route path="/admin/winner" component={AdminWinner} />
-					<Route path="/admin/users" component={AdminUsers} />
-					<Route path="/admin/prize" component={AdminPrize} />
-					<Route path="/admin/coupon" component={AdminCoupon} />
-				</Switch>
-			</BrowserRouter>
+			<AdminWrapper className="row">
+				<AdminMenu className="col-4 col-md-2">
+					<Menubar />
+				</AdminMenu>
+
+				<AdminActions className="col">
+					<Switch>
+						<Route exact path="/admin" component={AdminMain} />
+						<Route path="/admin/quiz" component={AdminQuiz} />
+						<Route path="/admin/participants" component={AdminParticipants} />
+						<Route path="/admin/scores" component={AdminScores} />
+						<Route path="/admin/winner" component={AdminWinner} />
+						<Route path="/admin/users" component={AdminUsers} />
+						<Route path="/admin/prize" component={AdminPrize} />
+						<Route path="/admin/coupon" component={AdminCoupon} />
+					</Switch>
+				</AdminActions>
+			</AdminWrapper>
 		)
 	}
 }
