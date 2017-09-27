ef2eee1315b227d827557fc478bd81ed529083ef
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2017-09-27 16:44:57 +0700

    Refactor FacebookLogin.js

--- src/components/User/FacebookLogin.js
+++ src/components/User/FacebookLogin.js
@@ -1,32 +1,17 @@
-import React from 'react'
-import styled from 'styled-components'
-import PropTypes from 'prop-types'
-import Button from 'material-ui/Button'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'material-ui/Button';
 
-const styles = {
-	faIcon: {
-		fontSize: '18px'
-	}
-}
-
-const Span = styled.span`margin-left: 10px;`
-
-const FacebookLogin = ({ facebookLogin, text }) => {
-	return (
-		<Button raised color="primary" onClick={facebookLogin}>
-			<i
-				className="fa fa-facebook-official"
-				aria-hidden="true"
-				style={styles.faIcon}
-			/>{' '}
-			<Span>{text}</Span>
-		</Button>
-	)
-}
+const FacebookLogin = ({ facebookLogin, text }) => (
+  <Button raised color="primary" onClick={facebookLogin}>
+    <i className="fa fa-facebook-official" aria-hidden="true" style={{ fontSize: '18px' }} />{' '}
+    <span style={{ marginLeft: '10px' }}>{text}</span>
+  </Button>
+);
 
 FacebookLogin.propTypes = {
-	facebookLogin: PropTypes.func,
-	text: PropTypes.string.isRequired
-}
+  facebookLogin: PropTypes.func,
+  text: PropTypes.string.isRequired,
+};
 
-export default FacebookLogin
+export default FacebookLogin;
