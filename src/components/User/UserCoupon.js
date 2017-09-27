712ca42d588a4fa35188a492b3c9f3773456330c
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2017-09-27 16:46:21 +0700

    eslint user coupon

--- src/components/User/UserCoupon.js
+++ src/components/User/UserCoupon.js
@@ -1,23 +1,21 @@
-import React from 'react'
-import styled from 'styled-components'
-import PropTypes from 'prop-types'
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 const Coupon = styled.span`
-	margin-top: 0px;
-	color: #424242;
-	font-weight: 500;
-`
+  margin-top: 0px;
+  color: #424242;
+  font-weight: 500;
+`;
 
-const UserCoupon = ({ coupon }) => {
-	return (
-		<Coupon>
-			จำนวนคูปอง : <i className="fa fa-ticket" aria-hidden="true" /> {coupon}
-		</Coupon>
-	)
-}
+const UserCoupon = ({ coupon }) => (
+  <Coupon>
+    จำนวนคูปอง : <i className="fa fa-ticket" aria-hidden="true" /> {coupon}
+  </Coupon>
+);
 
 UserCoupon.propTypes = {
-	coupon: PropTypes.number
-}
+  coupon: PropTypes.number,
+};
 
-export default UserCoupon
+export default UserCoupon;
