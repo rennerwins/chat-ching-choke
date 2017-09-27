f4ee6442c2a6c8aad90caf2f1139e0cc2b10c777
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2017-09-27 20:59:23 +0700

    eslint admin modules

--- src/modules/admin.js
+++ src/modules/admin.js
@@ -1,95 +1,94 @@
-import { db } from '../utils/firebase'
-import values from 'lodash/values'
-import keys from 'lodash/keys'
+import values from 'lodash/values';
+import keys from 'lodash/keys';
+import { db } from '../utils/firebase';
 
 // actions
-const TOTAL_USER = 'admin/TOTAL_USER'
-const TOTAL_COUPON = 'admin/TOTAL_COUPON'
-const TOTAL_PARTICIPANT = 'admin/TOTAL_PARTICIPANT'
-const SHOW_LATEST_USERS = 'admin/SHOW_LATEST_USERS'
+const TOTAL_USER = 'admin/TOTAL_USER';
+const TOTAL_COUPON = 'admin/TOTAL_COUPON';
+const TOTAL_PARTICIPANT = 'admin/TOTAL_PARTICIPANT';
+const SHOW_LATEST_USERS = 'admin/SHOW_LATEST_USERS';
 
 // action creators
-export const totalUsers = users => ({ type: TOTAL_USER, users })
+export const totalUsers = users => ({ type: TOTAL_USER, users });
 export const totalCoupon = coupons => ({
-	type: TOTAL_COUPON,
-	coupons
-})
+  type: TOTAL_COUPON,
+  coupons,
+});
 export const totalParticipant = participants => ({
-	type: TOTAL_PARTICIPANT,
-	participants
-})
-export const showLatestUsers = users => ({ type: SHOW_LATEST_USERS, users })
+  type: TOTAL_PARTICIPANT,
+  participants,
+});
+export const showLatestUsers = users => ({ type: SHOW_LATEST_USERS, users });
 
 // ajax
 export const getTotalUser = () => dispatch => {
-	db.ref('userIds').on('value', snapshot => {
-		let users = keys(snapshot.val()).length
-		dispatch(totalUsers(users))
-	})
-}
+  db.ref('userIds').on('value', snapshot => {
+    const users = keys(snapshot.val()).length;
+    dispatch(totalUsers(users));
+  });
+};
 
 export const getTotalCoupon = () => dispatch => {
-	db.ref('couponPair').on('value', snapshot => {
-		if (snapshot.val()) {
-			let coupons = snapshot.val().length
-			dispatch(totalCoupon(coupons))
-		}
-	})
-}
+  db.ref('couponPair').on('value', snapshot => {
+    if (snapshot.val()) {
+      const coupons = snapshot.val().length;
+      dispatch(totalCoupon(coupons));
+    }
+  });
+};
 
 export const getTotalParticipant = () => dispatch => {
-	db.ref('participants').on('value', snapshot => {
-		if (snapshot.val()) {
-			let participants = keys(snapshot.val()).length
-			dispatch(totalParticipant(participants))
-		}
-	})
-}
+  db.ref('participants').on('value', snapshot => {
+    if (snapshot.val()) {
+      const participants = keys(snapshot.val()).length;
+      dispatch(totalParticipant(participants));
+    }
+  });
+};
 
 export const getLatestUsers = amount => dispatch => {
-	db
-		.ref('users')
-		.limitToLast(amount)
-		.on('value', snapshot => {
-			dispatch(showLatestUsers(snapshot.val()))
-		})
-}
+  db
+    .ref('users')
+    .limitToLast(amount)
+    .on('value', snapshot => {
+      dispatch(showLatestUsers(snapshot.val()));
+    });
+};
 
 // reducers
 const initialState = {
-	latestUsers: []
-}
+  latestUsers: [],
+};
 
 const admin = (state = initialState, action) => {
-	switch (action.type) {
-		case TOTAL_USER:
-			return {
-				...state,
-				totalUsers: action.users
-			}
+  switch (action.type) {
+    case TOTAL_USER:
+      return {
+        ...state,
+        totalUsers: action.users,
+      };
 
-		case TOTAL_COUPON:
-			return {
-				...state,
-				totalCoupons: action.coupons
-			}
+    case TOTAL_COUPON:
+      return {
+        ...state,
+        totalCoupons: action.coupons,
+      };
 
-		case TOTAL_PARTICIPANT:
-			return {
-				...state,
-				totalParticipants: action.participants
-			}
+    case TOTAL_PARTICIPANT:
+      return {
+        ...state,
+        totalParticipants: action.participants,
+      };
 
-		case SHOW_LATEST_USERS:
-			let newUsers = values(action.users)
-			return {
-				...state,
-				latestUsers: newUsers
-			}
+    case SHOW_LATEST_USERS:
+      return {
+        ...state,
+        latestUsers: values(action.users),
+      };
 
-		default:
-			return state
-	}
-}
+    default:
+      return state;
+  }
+};
 
-export default admin
+export default admin;
