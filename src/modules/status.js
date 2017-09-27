56e799e50213549d6b9ca7fcfff617f0e0c3eca1
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2017-09-27 21:02:24 +0700

    eslint status modules

--- src/modules/status.js
+++ src/modules/status.js
@@ -1,49 +1,49 @@
-import { db } from '../utils/firebase'
+import { db } from '../utils/firebase';
 
 // actions
-const IS_PLAYING = 'status/IS_PLAYING'
-const CAN_ENTER = 'status/CAN_ENTER'
+const IS_PLAYING = 'status/IS_PLAYING';
+const CAN_ENTER = 'status/CAN_ENTER';
 
 // action creators
-export const isPlaying = status => ({ type: IS_PLAYING, status })
-export const canEnter = status => ({ type: CAN_ENTER, status })
+export const isPlaying = playing => ({ type: IS_PLAYING, playing });
+export const canEnter = entering => ({ type: CAN_ENTER, entering });
 
 // ajax
 export const checkPlaying = () => dispatch => {
-	db.ref('playing').on('value', snapshot => {
-		dispatch(isPlaying(snapshot.val()))
-	})
-}
+  db.ref('playing').on('value', snapshot => {
+    dispatch(isPlaying(snapshot.val()));
+  });
+};
 
 export const checkCanEnter = () => dispatch => {
-	db.ref('canEnter').on('value', snapshot => {
-		dispatch(canEnter(snapshot.val()))
-	})
-}
+  db.ref('canEnter').on('value', snapshot => {
+    dispatch(canEnter(snapshot.val()));
+  });
+};
 
 // reducers
 const initialState = {
-	playing: false,
-	canEnter: false
-}
+  playing: false,
+  canEnter: false,
+};
 
 const status = (state = initialState, action) => {
-	switch (action.type) {
-		case IS_PLAYING:
-			return {
-				...state,
-				playing: action.status
-			}
+  switch (action.type) {
+    case IS_PLAYING:
+      return {
+        ...state,
+        playing: action.status,
+      };
 
-		case CAN_ENTER:
-			return {
-				...state,
-				canEnter: action.status
-			}
+    case CAN_ENTER:
+      return {
+        ...state,
+        canEnter: action.status,
+      };
 
-		default:
-			return state
-	}
-}
+    default:
+      return state;
+  }
+};
 
-export default status
+export default status;
