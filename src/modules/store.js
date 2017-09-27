28a54f4c671fbf7fe830ffba9fab33b2d34afb5f
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2017-09-27 21:07:29 +0700

    eslint user modules

--- src/modules/store.js
+++ src/modules/store.js
@@ -1,24 +1,20 @@
-import { createStore, applyMiddleware, compose } from 'redux'
-import thunk from 'redux-thunk'
-import { combineReducers } from 'redux'
-import user from './user'
-import quiz from './quiz'
-import admin from './admin'
-import status from './status'
-import adminMessage from './adminMessage'
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+import user from './user';
+import quiz from './quiz';
+import admin from './admin';
+import status from './status';
+import adminMessage from './adminMessage';
 
 const rootReducer = combineReducers({
-	admin,
-	user,
-	quiz,
-	status,
-	adminMessage
-})
+  admin,
+  user,
+  quiz,
+  status,
+  adminMessage,
+});
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const store = createStore(
-	rootReducer,
-	composeEnhancers(applyMiddleware(thunk))
-)
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
-export default store
+export default store;
--- src/modules/user.js
+++ src/modules/user.js
@@ -1,151 +1,155 @@
-import * as api from '../utils/api'
-import { db, auth, facebookProvider } from '../utils/firebase'
+import * as api from '../utils/api';
+import { db, auth, facebookProvider } from '../utils/firebase';
 
 // actions
-const STORE_USER = 'user/STORE_USER'
-const REMOVE_USER = 'user/REMOVE_USER'
-const GET_USER_COUPON = 'user/GET_USER_COUPON'
+const STORE_USER = 'user/STORE_USER';
+const REMOVE_USER = 'user/REMOVE_USER';
+const GET_USER_COUPON = 'user/GET_USER_COUPON';
 
 // action creators
-export const storeUser = userDetails => ({ type: STORE_USER, userDetails })
-export const removeUser = () => ({ type: REMOVE_USER })
-export const getUserCoupon = coupon => ({ type: GET_USER_COUPON, coupon })
+export const storeUser = userDetails => ({ type: STORE_USER, userDetails });
+export const removeUser = () => ({ type: REMOVE_USER });
+export const getUserCoupon = coupon => ({ type: GET_USER_COUPON, coupon });
 
 // ajax
-export const getUserDetails = () => dispatch => {
-	auth.onAuthStateChanged(user => {
-		if (user) {
-			let { displayName, email, photoURL, uid } = user.providerData[0]
-			dispatch(
-				storeUser({
-					isLogin: true,
-					displayName,
-					email,
-					avatar: photoURL,
-					fbid: uid,
-					uid: user.uid
-				})
-			)
-			dispatch(checkAdmin(user.uid))
-			dispatch(addNewUserFromWeb(uid, user.uid))
-			localStorage.setItem('isLogin', true)
-		}
-	})
-}
-
 export const checkAdmin = firebaseID => dispatch => {
-	db.ref(`webAdmin/${firebaseID}`).once('value', snapshot => {
-		if (snapshot.val()) {
-			dispatch(storeUser({ isAdmin: true }))
-			localStorage.setItem('isAdmin', true)
-		} else {
-			dispatch(storeUser({ isAdmin: false }))
-			localStorage.removeItem('isAdmin')
-		}
-	})
-}
+  db.ref(`webAdmin/${firebaseID}`).once('value', snapshot => {
+    if (snapshot.val()) {
+      dispatch(storeUser({ isAdmin: true }));
+      localStorage.setItem('isAdmin', true);
+    } else {
+      dispatch(storeUser({ isAdmin: false }));
+      localStorage.removeItem('isAdmin');
+    }
+  });
+};
 
 export const addNewUserFromWeb = (facebookID, firebaseID) => async dispatch => {
-	const res = await api.addNewUserFromWeb(facebookID, firebaseID)
-	const { error_code, PSID, firstName, lastName, coupon } = await res
+  const res = await api.addNewUserFromWeb(facebookID, firebaseID);
+  const { PSID, firstName, lastName, coupon } = await res;
 
-	error_code === 555
-		? dispatch({ canPlay: false })
-		: dispatch(
-				storeUser({
-					PSID,
-					firstName,
-					lastName,
-					coupon,
-					canPlay: true,
-					loading: false
-				})
-			)
-}
+  if (res.errorCode === 555) {
+    dispatch({ canPlay: false });
+  } else {
+    dispatch(
+      storeUser({
+        PSID,
+        firstName,
+        lastName,
+        coupon,
+        canPlay: true,
+        loading: false,
+      })
+    );
+  }
+};
+
+export const getUserDetails = () => dispatch => {
+  auth.onAuthStateChanged(userDetail => {
+    if (userDetail) {
+      const { displayName, email, photoURL, uid } = userDetail.providerData[0];
+      dispatch(
+        storeUser({
+          isLogin: true,
+          displayName,
+          email,
+          avatar: photoURL,
+          fbid: uid,
+          uid: userDetail.uid,
+        })
+      );
+      dispatch(checkAdmin(userDetail.uid));
+      dispatch(addNewUserFromWeb(uid, userDetail.uid));
+      localStorage.setItem('isLogin', true);
+    }
+  });
+};
 
 export const facebookLogin = () => dispatch => {
-	facebookProvider.addScope('user_posts')
-	auth.signInWithPopup(facebookProvider).then(res => {
-		if (res) {
-			dispatch(storeUser({ isLogin: true }))
-			localStorage.setItem('isLogin', true)
-		}
-	})
-}
+  facebookProvider.addScope('user_posts');
+  auth.signInWithPopup(facebookProvider).then(res => {
+    if (res) {
+      dispatch(storeUser({ isLogin: true }));
+      localStorage.setItem('isLogin', true);
+    }
+  });
+};
 
 export const logout = () => dispatch => {
-	window.location.reload()
-	auth.signOut().then(() => {
-		dispatch(removeUser())
-		localStorage.removeItem('isAdmin')
-		localStorage.removeItem('isLogin')
-	})
-}
+  window.location.reload();
+  auth.signOut().then(() => {
+    dispatch(removeUser());
+    localStorage.removeItem('isAdmin');
+    localStorage.removeItem('isLogin');
+  });
+};
 
-export const checkParticipant = (user, quiz) => dispatch => {
-	const { PSID } = user
-	db.ref(`/participants/${PSID}`).once('value', snapshot => {
-		!snapshot.val() && dispatch(assignParticipant(user, quiz.quizList.length))
-	})
-}
+export const assignParticipant = (userDetail, quizLength) => {
+  const { PSID, firstName, lastName, avatar } = userDetail;
 
-export const assignParticipant = (user, quizLength) => dispatch => {
-	const { PSID, firstName, lastName, avatar } = user
+  const answerTemplate = Array(quizLength).fill({
+    ans: '',
+    correct: false,
+    at: 0,
+  });
 
-	let answerTemplate = Array(quizLength).fill({
-		ans: '',
-		correct: false,
-		at: 0
-	})
+  const tempParticipant = {
+    point: 0,
+    answerPack: answerTemplate,
+    firstName,
+    lastName,
+    profilePic: avatar,
+  };
 
-	let tempParticipant = {
-		point: 0,
-		answerPack: answerTemplate,
-		firstName,
-		lastName,
-		profilePic: avatar
-	}
+  db.ref(`participants/${PSID}`).set(tempParticipant);
+};
 
-	db.ref(`participants/${PSID}`).set(tempParticipant)
-}
+export const checkParticipant = (userDetail, quiz) => dispatch => {
+  const { PSID } = userDetail;
+  db.ref(`/participants/${PSID}`).once('value', snapshot => {
+    if (!snapshot.val) {
+      dispatch(assignParticipant(userDetail, quiz.quizList.length));
+    }
+  });
+};
 
 // reducers
 const userInitialDetails = {
-	loading: true,
-	displayName: '',
-	email: '',
-	avatar: '',
-	uid: '',
-	fbid: '',
-	PSID: '',
-	firstName: '',
-	lastName: '',
-	coupon: 0,
-	canPlay: false,
-	isAdmin: false,
-	isLogin: false
-}
+  loading: true,
+  displayName: '',
+  email: '',
+  avatar: '',
+  uid: '',
+  fbid: '',
+  PSID: '',
+  firstName: '',
+  lastName: '',
+  coupon: 0,
+  canPlay: false,
+  isAdmin: false,
+  isLogin: false,
+};
 
 const user = (state = userInitialDetails, action) => {
-	switch (action.type) {
-		case STORE_USER:
-			return {
-				...state,
-				...action.userDetails
-			}
+  switch (action.type) {
+    case STORE_USER:
+      return {
+        ...state,
+        ...action.userDetails,
+      };
 
-		case GET_USER_COUPON:
-			return {
-				...state,
-				coupon: action.coupon
-			}
+    case GET_USER_COUPON:
+      return {
+        ...state,
+        coupon: action.coupon,
+      };
 
-		case REMOVE_USER:
-			return userInitialDetails
+    case REMOVE_USER:
+      return userInitialDetails;
 
-		default:
-			return state
-	}
-}
+    default:
+      return state;
+  }
+};
 
-export default user
+export default user;
